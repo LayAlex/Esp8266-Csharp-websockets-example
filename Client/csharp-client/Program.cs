fd52cbc7c5a649133573e63e63c951ca8904ed21
Almir Abubekerov <kruiseabu@mail.ru>
2021-11-12 10:26:00 +0400

    rework client

---
+++ Client/csharp-client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebSocketSharp;
+
+
+namespace csharp_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebSocket ws = new WebSocket("ws://192.168.0.111:81/"))
+            {
+                ws.OnMessage += Ws_OnMessage;
+
+                ws.Connect();
+                ws.Send("Hello arduino");
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Received from the server: " + e.Data);
+        }
+    }
+}
